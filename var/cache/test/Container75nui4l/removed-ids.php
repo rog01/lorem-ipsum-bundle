4a02d83a94d41ff28ac9406e9d31ab202d22212f
roger <rogerjohary@yahoo.fr>
2020-01-23 15:32:18 +0100

    Chapter 12: Complex Config Test

--- var/cache/test/Container75nui4l/removed-ids.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'KnpU\\LoremIpsumBundle\\KnpUIpsum' => true,
-    'Psr\\Container\\ContainerInterface' => true,
-    'Symfony\\Component\\DependencyInjection\\ContainerInterface' => true,
-    'knpu_lorem_ipsum.knpu_word_provider' => true,
-    'knpu_lorem_ipsum.word_provider' => true,
-];
