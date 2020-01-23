8ca19fc12f2abcda7bc909a6a3fbff76dce845d9
roger <rogerjohary@yahoo.fr>
2020-01-23 14:49:21 +0100

    Chapter 11: Service Integration Test

---
+++ var/cache/test/Container75nui4l/removed-ids.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'KnpU\\LoremIpsumBundle\\KnpUIpsum' => true,
+    'Psr\\Container\\ContainerInterface' => true,
+    'Symfony\\Component\\DependencyInjection\\ContainerInterface' => true,
+    'knpu_lorem_ipsum.knpu_word_provider' => true,
+    'knpu_lorem_ipsum.word_provider' => true,
+];
